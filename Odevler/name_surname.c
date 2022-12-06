79afa2e94e9cfa6fbbbe30e095a4a511b287ea07
Miryakdemir <119675214+Miryakdemir@users.noreply.github.com>
2022-12-06 13:12:48 +0300

    Update name_surname.c
    Miray Akdemir

--- Odevler/name_surname.c
+++ Odevler/name_surname.c
@@ -1,1 +1,10 @@
 // Display your name and surname on screen 
+#include <stdio.h>
+int main(){
+  char Name[]=  "Miray";
+  char Surname[]= "Akdemir";
+  printf("%s", Name);
+  printf("%s",Surname);
+  return 0;
+
+}
